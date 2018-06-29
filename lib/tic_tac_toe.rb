35476419d61fc2c1ea025d10276f8d4fc5ebeed7
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-29 05:40:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ class TicTacToe
   !(@board[index].nil? || @board[index] == " ")
   end
 
-  def valid_move?(index)
+  def valid_move?
     if index.between?(0,8) && !position_taken?(index)
       true
     else
