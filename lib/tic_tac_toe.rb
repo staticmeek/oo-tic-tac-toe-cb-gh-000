fecfbe8248be767a97ffb059ab659371ef604895
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-29 05:30:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,8 +23,8 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index (user_input)
-  @user_input = user_input.to_i - 1
+  def input_to_index
+    @user_input = user_input.to_i - 1
   end
 
   def move (index, value)
