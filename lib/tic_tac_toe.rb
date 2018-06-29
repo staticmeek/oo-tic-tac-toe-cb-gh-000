a2b88cd49993af078bd416bd9d8d7c9b528be2d6
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-29 05:51:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,9 +75,9 @@ def won?
     position_3 = @board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return @win_combination
+      return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return @win_combination
+      return win_combination
     else
        false
     end
